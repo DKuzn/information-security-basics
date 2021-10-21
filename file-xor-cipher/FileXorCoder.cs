e52acf2a7a3c9287844838bf87f8a665e48ccc65
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-10-21 15:08:17 +0300

    Add file xor cipher
    Added FileXorCoder class and project files.

---
+++ file-xor-cipher/FileXorCoder.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+
+namespace file_xor_cipher
+{
+    class FileXorCoder 
+    {
+        private string code(string str, int key) 
+        {
+            string codedString = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                codedString += Convert.ToChar(str[i] ^ key);
+            }
+            return codedString;
+        }
+
+        private string openFile(string path)
+        {
+            FileStream file = new FileStream(path, FileMode.Open);
+            StreamReader strReader = new StreamReader(file);
+            string text = strReader.ReadToEnd();
+            strReader.Close();
+            file.Close();
+            return text;
+        }
+
+        private void saveFile(string text, string path)
+        {
+            FileStream file = new FileStream(path, FileMode.Create);
+            StreamWriter strWriter = new StreamWriter(file);
+            strWriter.Write(text);
+            strWriter.Close();
+        }
+
+        public void codeFile(string path)
+        {
+            string fileText = openFile(path);
+            string codedText = code(fileText, 12);
+            saveFile(codedText, path);
+        }
+    }
+}
